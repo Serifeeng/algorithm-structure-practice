8e90e4351992597ed07b35b6cde8b5fb76ef40c1
Serifeeng <serifeenginarrr@gmail.com>
2026-01-28 23:14:02 +0300

    Update fibonacci_recursive.py

--- recursion_backtracking/fibonacci_recursive.py
+++ recursion_backtracking/fibonacci_recursive.py
@@ -1,1 +1,26 @@
+def fibonacci_recursive(n):
+    """
+    Calculates the nth Fibonacci number using recursion.
+
+    Parameters:
+    n (int): Non-negative integer
+
+    Returns:
+    int: nth Fibonacci number
+    """
+
+    if n < 0:
+        raise ValueError("Fibonacci is not defined for negative numbers")
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+
+
+if __name__ == "__main__":
+    number = 10
+    print(fibonacci_recursive(number))
 
