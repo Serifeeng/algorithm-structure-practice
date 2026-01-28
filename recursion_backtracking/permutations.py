632bdfaa657125252da2b6da11ae2d9822a8748f
Serifeeng <serifeenginarrr@gmail.com>
2026-01-28 23:22:30 +0300

    Update permutations.py

--- recursion_backtracking/permutations.py
+++ recursion_backtracking/permutations.py
@@ -1,1 +1,36 @@
 
+def permutations(arr):
+    """
+    Generates all permutations of the given list.
+
+    Parameters:
+    arr (list): Input list
+
+    Returns:
+    list: List of all permutations
+    """
+
+    result = []
+    used = [False] * len(arr)
+    current = []
+
+    def backtrack():
+        if len(current) == len(arr):
+            result.append(current.copy())
+            return
+
+        for i in range(len(arr)):
+            if not used[i]:
+                used[i] = True
+                current.append(arr[i])
+                backtrack()
+                current.pop()
+                used[i] = False
+
+    backtrack()
+    return result
+
+
+if __name__ == "__main__":
+    data = [1, 2, 3]
+    print(permutations(data))
